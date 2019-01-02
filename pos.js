63d518afccd764a696098999a9b1401e9948e3bb
Alexandre Tolstenko <tolstenko@gmail.com>
2019-01-01 23:49:01 -0200

    gdcm js pure

--- pos.js
+++ pos.js
@@ -2,4 +2,10 @@
   return returndata;
 }
 
-module["exports"] = gdcmFunc;
+var ENVIRONMENT_IS_WEB = typeof window === 'object';
+var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
+var ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function' && !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_WORKER;
+var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
+
+if(ENVIRONMENT_IS_NODE)
+  module["exports"] = gdcmFunc;
