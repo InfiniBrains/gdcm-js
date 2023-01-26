3e718831f653ecd212e18735c54824e7f50993b0
Alexandre Tolstenko <tolstenko@gmail.com>
2023-01-25 23:01:27 -0500

    decompress dicom from memory

--- gdcmconvmod.cxx
+++ gdcmconvmod.cxx
@@ -16,6 +16,7 @@
 #include "gdcmImageChangePlanarConfiguration.h"
 #include "gdcmImageChangeTransferSyntax.h"
 #include "gdcmImageFragmentSplitter.h"
+#include "gdcmImageReader.h"
 #include "gdcmJPEG2000Codec.h"
 #include "gdcmJPEGCodec.h"
 #include "gdcmJPEGLSCodec.h"
@@ -24,6 +25,7 @@
 #include "gdcmReader.h"
 #include "gdcmSequenceOfFragments.h"
 #include "gdcmSequenceOfItems.h"
+#include "gdcmStreamImageReader.h"
 #include "gdcmSystem.h"
 #include "gdcmUIDGenerator.h"
 #include "gdcmVersion.h"
@@ -40,56 +42,31 @@
 #include <fstream>
 #include <sstream>
 
-struct SetSQToUndefined {
-  void operator()(gdcm::DataElement &de) { de.SetVLToUndefined(); }
-};
+//std::vector<uint8_t> fileToBytes(const std::string& filepath){
+//  std::basic_ifstream<uint8_t> fin(filepath, std::ios::in |
+//                                                   std::ios::binary);
+//  if(!fin.is_open()) {
+//    std::cout << "File can not be opened: " << filepath << std::endl;
+//    abort();
+//  }
+//
+//  auto buffer = std::vector<uint8_t>(std::istreambuf_iterator<uint8_t>(fin),
+//        std::istreambuf_iterator<uint8_t>());
+//  fin.close();
+//  return buffer;
+//}
 
-std::vector<uint8_t> decompressDicomBytes(std::vector<uint8_t> data) {
-  return std::vector<uint8_t>();
-}
+//void bytesToFile(std::vector<char> bytes, std::string filepath){
+//  std::ofstream fout(filepath, std::ios::out | std::ios::binary);
+//  if(!fout.is_open()) {
+//    std::cout << "File can not be opened: " << filepath << std::endl;
+//    abort();
+//  }
+//  fout.write(bytes.data(), bytes.size());
+//  fout.close();
+//}
 
-std::vector<uint8_t> fileToBytes(const std::string& filepath){
-  std::basic_ifstream<uint8_t> fin(filepath, std::ios::in |
-                                                   std::ios::binary);
-  if(!fin.is_open()) {
-    std::cout << "File can not be opened: " << filepath << std::endl;
-    abort();
-  }
-
-  auto buffer = std::vector<uint8_t>(std::istreambuf_iterator<uint8_t>(fin),
-        std::istreambuf_iterator<uint8_t>());
-  fin.close();
-  return buffer;
-}
-
-void bytesToFile(std::vector<uint8_t> bytes, std::string filepath){
-  std::ofstream fout(filepath, std::ios::out | std::ios::binary);
-  if(!fout.is_open()) {
-    std::cout << "File can not be opened: " << filepath << std::endl;
-    abort();
-  }
-  fout.write(reinterpret_cast<const char*>(bytes.data()), bytes.size());
-  fout.close();
-}
-
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    std::cout << "Usage: gdcmconvmod input.dcm output.dcm" << std::endl;
-    return 0;
-  }
-
-  std::string filename = argv[1];
-  std::string outfilename = argv[2];
-
-  if (filename.empty()) {
-    std::cerr << "Need input file\n";
-    return 1;
-  }
-  if (outfilename.empty()) {
-    std::cerr << "Need output file\n";
-    return 1;
-  }
-
+std::vector<char> decompressDicomBytes(std::vector<char> data) {
   // Debug is a little too verbose
   gdcm::Trace::SetDebug(false);
   gdcm::Trace::SetWarning(false);
@@ -97,19 +74,32 @@ int main(int argc, char *argv[]) {
 
   gdcm::FileMetaInformation::SetSourceApplicationEntityTitle("gdcmconv");
 
+  // convert bytes into stream
+  std::stringstream si;
+  si.rdbuf()->pubsetbuf(reinterpret_cast<char*>(&data[0]), data.size());
+
+  // read file
+  gdcm::File inputFile;
+  inputFile.Read(si);
+  auto& header = inputFile.GetHeader();
+  auto& dataset = inputFile.GetDataSet();
+
   gdcm::PixmapReader reader;
-  reader.SetFileName(filename.c_str());
+  reader.SetFile(inputFile);
+
+  // try read
   if (!reader.Read()) {
-    std::cerr << "Could not read (pixmap): " << filename << std::endl;
-    return 1;
+    std::cerr << "Could not read" << std::endl;
+    return {};
   }
-  gdcm::Pixmap &image = reader.GetPixmap();
+
+  gdcm::Pixmap &pixmap = reader.GetPixmap();
 
   gdcm::ImageChangeTransferSyntax change;
   change.SetForce(true);
   change.SetCompressIconImage(false);
 
-  const gdcm::TransferSyntax &ts = image.GetTransferSyntax();
+  const gdcm::TransferSyntax &ts = pixmap.GetTransferSyntax();
 
   if (ts.IsExplicit()) {
     change.SetTransferSyntax(gdcm::TransferSyntax::ExplicitVRLittleEndian);
@@ -117,31 +107,31 @@ int main(int argc, char *argv[]) {
     change.SetTransferSyntax(gdcm::TransferSyntax::ImplicitVRLittleEndian);
   }
 
-  change.SetInput(image);
+  change.SetInput(pixmap);
 
-  bool b = change.Change();
-  if (!b) {
-    std::cerr << "Could not change the Transfer Syntax: " << filename
-              << std::endl;
-    return 1;
+  if (!change.Change()) {
+    std::cerr << "Could not change the Transfer Syntax" << std::endl;
+    return {};
   }
+
   gdcm::PixmapWriter writer;
-  writer.SetFileName(outfilename.c_str());
   writer.SetFile(reader.GetFile());
 
-  gdcm::File &file = writer.GetFile();
-  gdcm::FileMetaInformation &fmi = file.GetHeader();
-//  fmi.Remove(
-//      gdcm::Tag(0x0002, 0x0100)); //  '   '    ' // PrivateInformationCreatorUID
-//  fmi.Remove(gdcm::Tag(0x0002, 0x0102)); //  '   '    ' // PrivateInformation
+  gdcm::File &outFile = writer.GetFile();
 
   const gdcm::Pixmap &pixout = change.PixmapToPixmapFilter::GetOutput();
   writer.SetPixmap(pixout);
+
+  std::stringstream so;
+  writer.SetStream(so);
+
   if (!writer.Write()) {
-    std::cerr << "Failed to write: " << outfilename << std::endl;
-    return 1;
+    std::cerr << "Failed to write" << std::endl;
+    return {};
   }
 
-  std::cout << "Written: " << outfilename << std::endl;
-  return 0;
+  std::cout << "Written" << std::endl;
+  auto str = so.str();
+  std::vector<char> ret(str.begin(), str.end());
+  return ret;
 }
